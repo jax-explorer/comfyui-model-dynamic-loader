839a3be2162b6b1ce155401474debe274e930600
jax <jaxinformationtransformer@gmail.com>
2024-12-15 14:27:31 +0800

    opt

--- __init__.py
+++ __init__.py
@@ -2,18 +2,22 @@
 from .nodes.LoadLoraFromCivitAI import LoadLoraFromCivitAIWithDownloader
 from .nodes.LoadLoraFromHF import LoadLoraFromHFWithDownloader
 from .nodes.LoadHunyuanLoraFromCivitAI import LoadHunyuanLoraFromCivitAIWithDownloader
+from .nodes.upload_anything import UploadAnything
 
 NODE_CLASS_MAPPINGS = { 
     "LoadLoraFromCivitAI":LoadLoraFromCivitAIWithDownloader,
     "LoadHunyuanLoraFromCivitAI":LoadHunyuanLoraFromCivitAIWithDownloader,
     "LoadLoraFromHF":LoadLoraFromHFWithDownloader,
+    "ComfyOnlineUploadAnything": UploadAnything,
 
 }
 NODE_DISPLAY_NAME_MAPPINGS = { 
     "LoadLoraFromCivitAI" : "Load Lora From CivitAI",
     "LoadLoraFromHF" : "Load Lora From HuggingFace",
     "LoadHunyuanLoraFromCivitAI": "Load HunyuanLora From CivitAI",
+    'ComfyOnlineUploadAnything': "ComfyOnlineUploadAnything"
 }
 
+WEB_DIRECTORY = "./web"
 
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', "WEB_DIRECTORY"]
