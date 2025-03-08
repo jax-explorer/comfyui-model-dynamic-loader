1fab7b9bc6792ce6ff563765bbc346e77d292447
jax <jaxinformationtransformer@gmail.com>
2025-03-09 00:24:31 +0800

    fix

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -12,7 +12,7 @@ import folder_paths
 import os
 
 folder_paths.add_model_folder_path("tmp_hunyuan_loras", os.path.join(folder_paths.models_dir, "tmp_hunyuan_loras"))
-folder_paths.add_model_folder_path("tmp_hunyuan_loras", os.path.join(folder_paths.models_dir, "tmp_hunyuan_loras"))
+folder_paths.add_model_folder_path("tmp_wanvideo_loras", os.path.join(folder_paths.models_dir, "tmp_wanvideo_loras"))
 
 
 class LoadWanVideoLoraFromCivitAIWithDownloader:
